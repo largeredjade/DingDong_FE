cc637b1d16726132ac0a30f9de1b59fb5fe10a9c
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-04-27 19:23:40 +0900

    setting: global styles 설정

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <GlobalStyles/>
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+const size = {
+    phone: "393px",
+    tablet: "640px",
+    laptop: "1200px",
+    desktop: "1800px",
+};
+
+const theme = {
+    colors :{
+        mainColor: "#6ABD8C",
+        mainColorLight: "#CCDFB0",
+        backgroundColor: "#F6F9F0",
+        darkGray: "#595959",
+        lightGray: "#939292",
+        superLightGray: "#F1F2F5",
+    },
+    backgroundColor: {
+        lightGray: "#939292",
+        superLightGray: "#F1F2F5",
+    },
+
+};
+
+export default theme;
