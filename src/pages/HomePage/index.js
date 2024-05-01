99a687b9850cd276b1ecb54697fd17c8712336eb
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-01 14:55:50 +0900

    feat: 홈페이지 ui, main 페이지 이동

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,19 @@
+import styled from "styled-components";
+import {Logo} from "../../components/Icons/logos";
+import {Link} from "react-router-dom";
 import GoToTestBtn from "../../components/HomePage/GoToTestBtn";
-
 function HomePage() {
     return (
         <>
-            <h1>
-                홈페이지 입니다.
-            </h1>
             <GoToTestBtn/>
+            <Link to={'/main'}>
+                <DomWrap>
+                    <ItemWrapper>
+                        <Logo/>
+                        <p>우리 서비스 슬로건</p>
+                    </ItemWrapper>
+                </DomWrap>
+            </Link>
 
         </>
 
@@ -14,3 +21,18 @@ function HomePage() {
 }
 
 export default HomePage;
+const DomWrap = styled.div`
+    margin-top: 55px;
+    border-radius: 400px 400px 0 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${({theme}) => theme.backgroundColor.mainColor};
+    @media (max-width: ${({theme})=> theme.mobile}) {
+        border-radius: 200px 200px 0 0;
+    }
+`
+
+const ItemWrapper = styled.div`
+    `
