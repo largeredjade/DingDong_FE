2efb068d8bf767e3feb8daf2cc5ee013e8b0fb62
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-06 00:54:51 +0900

    css: 로그인 팝업창

---
+++ src/components/Popup/LoginPopup.js
@@ -0,0 +1,96 @@
+import {useState} from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+function LoginPopup() {
+
+    return (
+        <>
+            <Wrapper>
+                <PopupContent>
+                    <InformLogin>
+                        <p>로그인이 필요합니다.</p>
+                        <p>로그인 하시겠습니까?</p>
+                        <Link to={'/join'}>
+                            <span>회원가입</span>
+                        </Link>
+                        <BtnContainer>
+                        <Btn>취소</Btn>
+                            <Link to={'/login'}>
+                                <Btn>확인</Btn>
+                            </Link>
+                        </BtnContainer>
+                    </InformLogin>
+                </PopupContent>
+            </Wrapper>
+        </>
+
+    );
+}
+
+export default LoginPopup;
+
+
+const Wrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const PopupContent = styled.div`
+    background: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 30px;
+    border: 2px solid ${({theme})=>theme.colors.mainColorDark};
+    text-align: center;
+    width: 350px;
+    height: 278px;
+`;
+
+const InformLogin = styled.div`
+    margin-top: 20px;
+    p{
+        color: ${({theme})=> theme.colors.darkGray};
+        font-weight: bold;
+        font-size: 25px;
+        margin: 10px;
+    }
+    span{
+        color: ${({theme})=> theme.colors.lightGray};
+        font-size: 20px;
+        text-decoration: underline;
+        text-decoration-thickness: 1px;
+
+    }
+`
+
+const BtnContainer = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+`;
+
+const Btn = styled.button`
+    margin: 10px;
+    background:  ${({theme})=> theme.backgroundColor.mainColor};
+    width: 110px;
+    height: 60px;
+    border-radius: 20px;
+    color: ${({theme})=>theme.colors.darkGray};
+    font-weight: bold;
+    font-size: 20px;
+    &:hover{
+        background:  ${({theme})=> theme.backgroundColor.mainColorDark};
+
+    }
+`
+
