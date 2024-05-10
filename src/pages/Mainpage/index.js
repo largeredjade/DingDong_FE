a84b734d88173a14f907e4ff0131dc3e22330485
sujihong <sujihong93@gmail.com>
2024-05-10 17:13:27 +0900

    메인페이지 인덱스 작성

--- src/pages/Mainpage/index.js
+++ src/pages/Mainpage/index.js
@@ -1,10 +1,55 @@
+import PageHeader from "../../components/PageHeader/PageHeader";
+import MainItems from "../../components/MainPage/MainItems";
+import styled from "styled-components";
+
 function MainPage() {
     return (
-        <>
-            <div>메인페이지 입니다.</div>
+        <> 
+            <Wrap>
+               <PageHeader/>
+               <MaincontentBox>
+                <NowBtnBox>
+                    <NowBtn>현재 모집중</NowBtn>
+                </NowBtnBox>
+                <ItemBox>
+                    <MainItems/>
+                </ItemBox>
+               </MaincontentBox>
+               
+            </Wrap>
         </>
 
     );
 }
 
 export default MainPage;
+
+const Wrap = styled.div`
+
+`;
+
+const MaincontentBox = styled.div`
+    display: grid;
+    grid-row: 2;
+    justify-content: center;
+    margin-top: 200px;
+`;
+
+
+const NowBtnBox = styled.div`
+    text-align: right;
+    justify-content: flex-end;
+    margin-bottom: 20px;
+`;
+
+const NowBtn = styled.button`
+    width: 112px;
+    height: 32px;
+    background: #FFFFFF;
+    border: 1px solid #B7B7B7;
+    border-radius: 20px;
+`
+
+const ItemBox = styled.div`
+
+`;
