c61c8169574d3aea8a96e9593a6e4843c6d42a4f
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-12 22:08:24 +0900

    setting:쿠키, 로컬스토리지 설정

---
+++ src/auth/localStorage.js
@@ -0,0 +1,10 @@
+// 사용자 정보를 로컬 스토리지에 저장하는 함수
+export const saveUserInfoToLocalStorage = (userInfo) => {
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+};
+
+// 로컬 스토리지에서 사용자 정보를 가져오는 함수
+export const getUserInfoFromLocalStorage = () => {
+    const userInfoString = localStorage.getItem('userInfo');
+    return userInfoString ? JSON.parse(userInfoString) : null;
+};
