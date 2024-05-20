b29e44caab3c1f206a17d773cdcbcf9c56dfcbfe
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-20 14:42:06 +0900

    feat: 큐알 스캐너 팝업

---
+++ src/components/Popup/QrScanPopup.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import QrScanner from "react-qr-scanner";
+import styled from "styled-components";
+
+
+const QrScanPopup = () => {
+    const [scannedText, setScannedText] = useState("");
+    const [isOpen, setIsOpen] = useState(true);
+
+    const handleScan = (data) => {
+        if (data) {
+            setScannedText(data.text);
+            console.log(scannedText);
+            setIsOpen(false)
+        //    유저 이름 백엔드에 넘겨줄것
+        }
+    };
+
+    const handleError = (err) => {
+        console.error(err);
+    };
+
+    return (
+        <>
+            {isOpen &&
+                <Wrapper>
+                <QrScanner
+                    onScan={handleScan}
+                    onError={handleError}
+                    style={{ width: "100%", height: "100%" }}
+                />
+                <p>{scannedText}</p>
+            </Wrapper>}
+        </>
+
+    );
+};
+
+export default QrScanPopup;
+
+
+const Wrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+`;
