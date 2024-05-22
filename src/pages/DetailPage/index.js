89f81a15e22621b291839bb5a7507dc3be056cc5
norangsuji <sujihong93@gmail.com>
2024-05-22 18:45:07 +0900

    동아리 상세 페이지 먹데이터 연결

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,18 +1,17 @@
 import PageHeader from "../../components/PageHeader/PageHeader";
 import DetailItems from "../../components/DetailPage/DetailItems";
 import styled from "styled-components";
+import clubData from '../../mockdata/clubDetailpage/club_detail.json';
 
 function DetailPage() {
+  const clubDetails = [clubData]
   return (
     <>
       <Wrap>
         <PageHeader />
         <DetailcontentBox>
-          <ShowAllBtnBox>
-            <ShowAllBtn>전체 보기</ShowAllBtn>
-          </ShowAllBtnBox>
           <ItemBox>
-            <DetailItems />
+            <DetailItems clubDetails={clubDetails}/>
           </ItemBox>
         </DetailcontentBox>
       </Wrap>
@@ -32,20 +31,6 @@ const DetailcontentBox = styled.div`
   grid-row: 2;
   justify-content: center;
   margin-top: 200px;
-`;
-
-const ShowAllBtnBox = styled.div`
-  text-align: right;
-  justify-content: flex-end;
-  margin-bottom: 20px;
-`;
-
-const ShowAllBtn = styled.button`
-  width: 112px;
-  height: 32px;
-  background: #ffffff;
-  border: 1px solid #b7b7b7;
-  border-radius: 20px;
 `;
 
 const ItemBox = styled.div``;
