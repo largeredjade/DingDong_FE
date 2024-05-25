6b6e717287cfba92f636ecd12a0c6cb45f0bc442
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-25 17:56:33 +0900

    docs: 인스턴스 url 수정

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `http://dingdong.com/api`
+    baseURL: `https://dingdong7.pythonanywhere.com`
 });
 
 export default instance;
