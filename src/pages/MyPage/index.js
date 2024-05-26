267de34b51fa89db1bcec5cb806da75fa5b1721a
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-26 16:49:50 +0900

    test: 마이페이지 api 연동시 사용할 user_id 테스트

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -3,9 +3,14 @@ import MyPageItemes from "../../components/MyPage/MyPageItems";
 import MyPageClubItems from "../../components/MyPage/MyPageClubItems";
 import styled from "styled-components";
 import manager_user from "../../mockdata/mypage/manager_user.json"
+import {getCookie} from "../../auth/cookie";
 
 function MyPage() {
     const user_data = [manager_user];
+    const user_id = getCookie('user_id')
+    console.log('user_id::',user_id);
+    console.log('api 연동 시 사용할 주소::', `https://dingdong7.pythonanywhere.com/mypage/${user_id}`);
+
     return (
         <>  <Wrap>
                <PageHeader/>
