fb62d07060ca3eb1f5e0050d2273ef968dbdbc19
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-27 16:12:21 +0900

    docs: aixios  Instance 수정

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `https://dingdong7.pythonanywhere.com`
+    baseURL: `https://dingdong7.pythonanywhere.com`,
+    withCredentials: true
 });
 
 export default instance;
