eee0baa3bba2885c90717c3d7398f8a9089d56ff
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-28 21:24:39 +0900

    api: 동아리 등록 연동

--- src/components/Popup/JoinClubPopup.js
+++ src/components/Popup/JoinClubPopup.js
@@ -1,37 +1,72 @@
 import styled from "styled-components";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import axiosInstance from "../../lib/axios";
+import {getCookie} from "../../auth/cookie";
 
-function JoinClubPopup() {
+function JoinClubPopup({club_id}) {
+    const [isOpen, setIsOpen] =useState(true);
     const [code,setCode] = useState('')
     const navigation = useNavigate()
+    const accessToken = getCookie('access')
+
 
     const handleCode = (e)=>{
         setCode(e.target.value)
-        console.log('e.target.value:',e.target.value);
     }
 
-    const handleSubmit = ()=>{
-        if(code == ''){
-            alert('다시 입력해 주세요.')
-        }else{
-            alert('가입 성공!')
-            navigation('/main')
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        try {
+            const response = await axiosInstance.post(`/clubs/${club_id}/club_code/`, {
+                club_code_enter: code,
+            },{
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                }
+            });
+            console.log('Response:', response);
+            alert('가입 성공!');
+            navigation('/main');
+        }  catch (error) {
+            console.error('가입 오류:', error);
+            if (error.response && error.response.status === 400) {
+                if(error.response.data.detail ==='You are already a member of this club.'){
+                    alert('이미 등록된 회원입니다.')
+                }
+                if(error.response.data.detail ==='The club code is incorrect.'){
+                    alert('잘못된 가입 코드입니다. 다시 입력해 주세요.')
+                }
+            }
         }
     }
+
+    const handleCancel = ()=>{
+        setIsOpen(false)
+    }
+
+
     return (
         <>
-            <Wrapper>
-                <PopupContent>
-                    <InformJoin>
-                        <p>동아리 가입코드 입력이 필요합니다.</p>
-                        <WriteInput
-                            onChange={handleCode}
-                            placeholder={'동아리 가입코드를 입력해 주세요.'}/>
-                        <OkBtn onClick={handleSubmit}>확인</OkBtn>
-                    </InformJoin>
-                </PopupContent>
-            </Wrapper>
+            {isOpen &&
+                <Wrapper>
+                    <PopupContent>
+                        <InformJoin>
+                            <p>동아리 가입코드 입력이 필요합니다.</p>
+                            <WriteInput
+                                onChange={handleCode}
+                                placeholder={'동아리 가입코드를 입력해 주세요.'}/>
+                            <BtnContainer>
+                                <Btn onClick={handleSubmit}>확인</Btn>
+                                <Btn onClick={handleCancel}>닫기</Btn>
+                            </BtnContainer>
+
+                        </InformJoin>
+                    </PopupContent>
+                </Wrapper>
+            }
+
         </>
 
     );
@@ -84,8 +119,16 @@ const WriteInput = styled.input`
     font-size: 16px;
     padding-left: 16px;
 `
-const OkBtn = styled.button`
-    margin-top: 15px;
+
+const BtnContainer = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+`;
+
+const Btn = styled.button`
+    margin: 10px;
     background:  ${({theme})=> theme.backgroundColor.mainColor};
     width: 110px;
     height: 60px;
@@ -95,6 +138,6 @@ const OkBtn = styled.button`
     font-size: 20px;
     &:hover{
         background:  ${({theme})=> theme.backgroundColor.mainColorDark};
-        
     }
 `
+
