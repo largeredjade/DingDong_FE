2e052eb55aa51b3dfcc3aa981113b4aa9b37aef3
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-29 20:20:19 +0900

    docs: 쓸모없는 코드 삭제

--- src/auth/cookie.js
+++ src/auth/cookie.js
@@ -2,9 +2,6 @@ import {Cookies} from "react-cookie";
 
 const cookies = new Cookies()
 
-// export const setCookie = (name, value, options) =>{
-//     return cookies.set(name,value,{...options})
-// }
 
 export const setCookie = (name, value) =>{
     return cookies.set(name,value)
