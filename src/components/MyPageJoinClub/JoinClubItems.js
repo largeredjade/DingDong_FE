d8a0764a093e350b3119f7b2a3353698826b76e3
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-30 04:10:07 +0900

    api: 가입한 동아리 보여주기 props 적용

--- src/components/MyPageJoinClub/JoinClubItems.js
+++ src/components/MyPageJoinClub/JoinClubItems.js
@@ -8,26 +8,20 @@ import axiosInstance from '../../lib/axios'
 function JoinClubItems() {
     let navigate = useNavigate();
     const params = useParams();
+    const [userData, setUserData] = useState();
+
+
     console.log(params);
+    console.log(userData);
 
     const accessToken = getCookie('access');
     const user_id = getCookie('user_id');
     const club_id = params.club_id;
     const qr_id =params.qr_id;
-    
+
     console.log(params.club_id);
     console.log(params.qr_id);
 
-
-    const [userData, setUserData] = useState();
-    const [clubData, setClubData] = useState({
-        status: "",
-        club_id: 0,
-        message: "",
-        attendance_date: "",
-        user_id: 0,
-        username: ""
-    });
     const [showQrScan, setShowQrScan] = useState(false);
 
     async function fetchUserData() {
@@ -44,22 +38,8 @@ function JoinClubItems() {
         }
     }
 
-    async function handleQrScan() {
-        setShowQrScan(!showQrScan);
-        if (!showQrScan) {
-            try {
-                const response = await axiosInstance.put(`/mypage/clubs/${club_id}/scan_qr/${qr_id}`, {
-                    ...clubData
-                }, {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`
-                    }
-                });
-                console.log('출석 정보 전송 응답:', response);
-            } catch (error) {
-                console.error("출석 정보 전송 오류:", error);
-            }
-        }
+    const handleQrScan = ()=>{
+        setShowQrScan(!showQrScan)
     }
 
     useEffect(() => {
@@ -71,13 +51,13 @@ function JoinClubItems() {
             {userData && (
                 <Wrapper key={userData.user_id}>
                     <ItemBox>
-                        <JoinClubinfo>내가 가입한 동아리 {userData.registered_clubs.map((i) => (
+                        <JoinClubinfo>내가 가입한 동아리 {userData.joined_clubs.map((i) => (
                             <span key={i.id}>{i.name}</span>
                         ))}</JoinClubinfo>
                     </ItemBox>
                     <BtnItemBox>
                         <BtnCheckQR onClick={handleQrScan}><p>출석 QR<br />스캔하기</p></BtnCheckQR>
-                        {showQrScan && <QrScanPopup />}
+                        {showQrScan && <QrScanPopup data={params.club_id} data1={params.qr_id}/>}
                     </BtnItemBox>
                 </Wrapper>
             )}
@@ -110,7 +90,7 @@ const BtnItemBox = styled.div`
     justify-content: center;
     align-items: center;
     color: ${({theme})=> theme.colors.darkGray};
-    
+
 `;
 const BtnCheckQR = styled.button`
     p{
