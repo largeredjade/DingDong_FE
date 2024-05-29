1d230929665b3c58578ad2d542376e1aef7e90c9
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-30 04:07:39 +0900

    api: 클럽조인 get api 연동

--- src/pages/MyPageJoinClub/index.js
+++ src/pages/MyPageJoinClub/index.js
@@ -2,22 +2,55 @@ import PageHeader from "../../components/PageHeader/PageHeader";
 import JoinClubItems from"../../components/MyPageJoinClub/JoinClubItems";
 import JoinClubCalendar from"../../components/MyPageJoinClub/Calendar";
 import styled from "styled-components";
-import JoinClubDetail from '../../mockdata/mypage/joined_club_detail.json';
-import manager_user from "../../mockdata/mypage/manager_user.json"
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../lib/axios";
 import {getCookie} from "../../auth/cookie";
 import { useEffect, useState} from 'react';
+import Loading from "../../components/LoadingSpinner/Loading";
 
 function MyPageJoinClub() {
+    const params = useParams();
+    const user_id = getCookie('user_id');
+    const accessToken = getCookie('access');
+    console.log(params)
+    const club_id = params.club_id;
+    console.log(params.club_id)
+
+    const [data, setUserData] = useState();
+
+    async function ClubData() {
+        try {
+            const response = await axiosInstance.get(`mypage/clubs/${club_id}/attendance/${user_id}/`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            console.log(response);
+            setUserData(response.data);
+        } catch (error) {
+            console.error("API 호출 오류:", error);
+        }
+    }
+    useEffect(() => {
+        ClubData();
+    }, []);
+
     return (
         <>
-        
+
             <Wrap>
-               <PageHeader/>
-                <ItemBox>
-                    <JoinClubItems/>
-                </ItemBox>
+                <PageHeader/>
+                {
+                    data ?
+                        <>
+                            <ItemBox>
+                                <JoinClubItems/>
+                            </ItemBox>
+                            <JoinClubCalendar data={data} params={params}/>
+                        </>
+                        :
+                        <Loading/>
+                }
             </Wrap>
         </>
 
